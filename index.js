59a6724fd3f18bba3abca5c7f4ed4788e23a4178
lystyp <lystyp87015@gmail.com>
2017-07-31 17:57:51 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,118 +1,49 @@
-// 'use strict';
-
-// const ActionsSdkApp = require('actions-on-google').ActionsSdkApp;
-
-// exports.<insertCloudFunctionName>= (req, res) => {
-//   const app = new ActionsSdkApp({request: req, response: res});
-
-//     function mainIntent (app) {
-//       let inputPrompt = app.buildInputPrompt(false,
-//         'Hi! Say something, and I\'ll repeat it');
-//       app.ask(inputPrompt);
-//     }
-
-//     function respond (app) {
-//       let inputPrompt = app.buildInputPrompt(false,
-//         'Hi! Say something, and I\'ll repeat it.');
-//       app.ask(inputPrompt);
-//     }
-
-// }
-
-// let actionMap = new Map();
-// actionMap.set(app.StandardIntents.MAIN, mainIntent);
-// actionMap.set(app.StandardIntents.TEXT, respond);
-
-// //add more intents to the map
-
-// app.handleRequest(actionMap);
-
-
-
-
-
-
-
-
 'use strict';
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const ActionsSdkApp = require('actions-on-google').ActionsSdkApp;
 
-const restService = express();
+exports.<insertCloudFunctionName>= (req, res) => {
+  const app = new ActionsSdkApp({request: req, response: res});
 
-restService.use(bodyParser.urlencoded({
-    extended: true
-}));
-
-restService.use(bodyParser.json());
-
-restService.post('', function(req, res) {
-    var speech = "Tets. Speak again."
-    return res.json({
-        speech: speech,
-        displayText: speech,
-        source: 'webhook-echo-sample'
-    });
-});
-
-restService.post('/slack-test', function(req, res) {
-
-    var slack_message = {
-        "text": "Details of JIRA board for Browse and Commerce",
-        "attachments": [{
-            "title": "JIRA Board",
-            "title_link": "http://www.google.com",
-            "color": "#36a64f",
-
-            "fields": [{
-                "title": "Epic Count",
-                "value": "50",
-                "short": "false"
-            }, {
-                "title": "Story Count",
-                "value": "40",
-                "short": "false"
-            }],
-
-            "thumb_url": "https://stiltsoft.com/blog/wp-content/uploads/2016/01/5.jira_.png"
-        }, {
-            "title": "Story status count",
-            "title_link": "http://www.google.com",
-            "color": "#f49e42",
-
-            "fields": [{
-                "title": "Not started",
-                "value": "50",
-                "short": "false"
-            }, {
-                "title": "Development",
-                "value": "40",
-                "short": "false"
-            }, {
-                "title": "Development",
-                "value": "40",
-                "short": "false"
-            }, {
-                "title": "Development",
-                "value": "40",
-                "short": "false"
-            }]
-        }]
+    function mainIntent (app) {
+      let inputPrompt = app.buildInputPrompt(false,
+        'Hi! Say something, and I\'ll repeat it');
+      app.ask(inputPrompt);
     }
-    return res.json({
-        speech: "speech",
-        displayText: "speech",
-        source: 'webhook-echo-sample',
-        data: {
-            "slack": slack_message
-        }
-    });
-});
+
+    function respond (app) {
+      let inputPrompt = app.buildInputPrompt(false,
+        'Hi! Say something, and I\'ll repeat it.');
+      app.ask(inputPrompt);
+    }
+
+}
+
+let actionMap = new Map();
+actionMap.set(app.StandardIntents.MAIN, mainIntent);
+actionMap.set(app.StandardIntents.TEXT, respond);
+
+//add more intents to the map
+
+app.handleRequest(actionMap);
+
+
+function responseHandler (app) {
+  // intent contains the name of the intent you defined in `initialTriggers`
+  let intent = app.getIntent();
+  switch (intent) {
+    case app.StandardIntents.MAIN:
+      app.ask('Welcome! Say a number.');
+      break;
+
+    case app.StandardIntents.TEXT:
+      let number = app.getArgument(NUMBER_ARGUMENT);
+      app.tell('You said ' + number);
+      break;
+  }
+}
+// you can add the function name instead of an action map
+app.handleRequest(responseHandler);
 
 
 
-
-restService.listen((process.env.PORT || 8000), function() {
-    console.log("Server up and listening");
-});
