3cf993336986caf0104acdab162ff322746b4596
lystyp <lystyp87015@gmail.com>
2017-07-31 18:04:45 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,50 +1,55 @@
+// Copyright 2016, Google, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 'use strict';
 
+process.env.DEBUG = 'actions-on-google:*';
+
 const ActionsSdkApp = require('actions-on-google').ActionsSdkApp;
+const functions = require('firebase-functions');
 
-exports.<insertCloudFunctionName>= (req, res) => {
-  console.log("TESTTTTTTTTTTTT");
-  const app = new ActionsSdkApp({request: req, response: res});
+const NO_INPUTS = [
+  'I didn\'t hear that.',
+  'If you\'re still there, say that again.',
+  'We can stop here. See you soon.'
+];
 
-    function mainIntent (app) {
-      let inputPrompt = app.buildInputPrompt(false,
-        'Hi! Say something, and I\'ll repeat it');
-      app.ask(inputPrompt);
-    }
+exports.sayNumber = functions.https.onRequest((request, response) => {
+  const app = new ActionsSdkApp({request, response});
 
-    function respond (app) {
-      let inputPrompt = app.buildInputPrompt(false,
-        'Hi! Say something, and I\'ll repeat it.');
-      app.ask(inputPrompt);
-    }
-
-}
-
-let actionMap = new Map();
-actionMap.set(app.StandardIntents.MAIN, mainIntent);
-actionMap.set(app.StandardIntents.TEXT, respond);
-
-//add more intents to the map
-
-app.handleRequest(actionMap);
-
-
-function responseHandler (app) {
-  // intent contains the name of the intent you defined in `initialTriggers`
-  let intent = app.getIntent();
-  switch (intent) {
-    case app.StandardIntents.MAIN:
-      app.ask('Welcome! Say a number.');
-      break;
-
-    case app.StandardIntents.TEXT:
-      let number = app.getArgument(NUMBER_ARGUMENT);
-      app.tell('You said ' + number);
-      break;
+  function mainIntent (app) {
+    console.log('mainIntent');
+    let inputPrompt = app.buildInputPrompt(true, '<speak>Hi! <break time="1"/> ' +
+      'I can read out an ordinal like ' +
+      '<say-as interpret-as="ordinal">123</say-as>. Say a number.</speak>', NO_INPUTS);
+    app.ask(inputPrompt);
   }
-}
-// you can add the function name instead of an action map
-app.handleRequest(responseHandler);
 
+  function rawInput (app) {
+    console.log('rawInput');
+    if (app.getRawInput() === 'bye') {
+      app.tell('Goodbye!');
+    } else {
+      let inputPrompt = app.buildInputPrompt(true, '<speak>You said, <say-as interpret-as="ordinal">' +
+        app.getRawInput() + '</say-as></speak>', NO_INPUTS);
+      app.ask(inputPrompt);
+    }
+  }
 
+  let actionMap = new Map();
+  actionMap.set(app.StandardIntents.MAIN, mainIntent);
+  actionMap.set(app.StandardIntents.TEXT, rawInput);
 
+  app.handleRequest(actionMap);
+});
